25e6e38ac7d097dce0e6e4dfe9abf93698613345
Choi Soon Woo <asdd2557@naver.com>
2021-09-24 21:55:10 +0900

    포스트 상세 페이지 부트스트랩 적용하기

--- do_it_django_prj/urls.py
+++ do_it_django_prj/urls.py
@@ -17,7 +17,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('blog', include('blog.urls')),
     path('admin/', admin.site.urls),
-    path('',include('single_pages.urls')),
+    path('', include('single_pages.urls')),
+    path('blog', include('blog.urls')),
+
+
 ]
