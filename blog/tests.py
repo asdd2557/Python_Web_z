87959f3ba156000f9a1c6458084969373e974e9a
asdd2557 <asdd2557@naver.com>
2021-10-29 01:13:59 +0900

    author 필드 추가하기

--- blog/tests.py
+++ blog/tests.py
@@ -19,7 +19,11 @@ class TestView(TestCase):
         self.assertEqual(soup.title.text, 'Blog')              #그리고 페이지의 title Text만 가져와 그 텍스타가 Blog인지 확인 합니다.
 
         #1.4 내비게이션 바가 있다.
-        navbar = soup.nav #soup.nav의 명령어로 soup의 네비게이션(nav)만 가져와 navbar에 담습니다.
+        def navbar_test(self, soup):
+            navbar = soup.nav  # soup.nav의 명령어로 soup의 네비게이션(nav)만 가져와 navbar에 담습니다.
+            self.assertIn('Blog', navbar.text)
+            self.assertIn('About Me', navbar.text)
+
 
         #1.5 Blog, About Me 라는 문구가 내비게이션 바에 있다.
         self.assertIn('Blog', navbar.text)       #위에 담았던 nav 바에서 텍스트가 blog인지 확인 한다.
@@ -72,15 +76,17 @@ class TestView(TestCase):
         self.assertEqual(response.status_code, 200)
         soup = BeautifulSoup(response.content, 'html.parser')
 
+
         # 2.2 포스트 목록 페이지와 똑같은 내비게이션 바가 있다.
-        navbar = soup.nav
-        self.assertIn('Blog', navbar.text)
-        self.assertIn('About Me', navbar.text)
+        self.navbar_test(soup)
+
+
 
         # 2.3 첫 번째 포스트의 제목이 웹 브라우저 탭 타이틀에 들어있다.
         self.assertIn(post_001.title,soup.title.text)
 
         # 2.4 첫 번째 포스트의 제목이 포스트 영역(post_area)에 있다.
+
         main_area = soup.find('div', id = 'main_area')
         post_area = main_area.find('div', id = 'post_area')
         self.assertIn(post_001.title, post_area.text)
@@ -90,4 +96,16 @@ class TestView(TestCase):
 
         # 2.6 첫 번째 포스트의 내용(content)이 포스트 영역에 있다.
         self.assertIn(post_001.content ,post_area.text)
+
+        logo_btn = navbar.find('a',text='Do It Django')
+        self.assertEqual(logo_btn.attrs['href'], '/')
+
+        home_btn = navbar.find('a', text='Home')
+        self.assertEqual(home_btn.attrs['href'], '/')
+
+        blog_btn = navbar.find('a', text='Blog')
+        self.assertEqual(blog_btn.attrs['href'], '/blog/')
+
+        about_me_btn = navbar.find('a', text='About Me')
+        self.assertEqual(about_me_btn.attrs['href'], '/about_me/')
 # Create your tests here.
