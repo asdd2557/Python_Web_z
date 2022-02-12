e050c222ee1afcd8138b199166907c82698f57e8
asdd2557 <asdd2557@naver.com>
2022-02-12 15:11:42 +0900

    태그 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render
-from .models import Post, Category
+from .models import Post, Category, Tag
 
 
 class PostList(ListView):
@@ -13,6 +13,22 @@ class PostList(ListView):
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
+
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+    post_list = tag.post_set.all()
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'tag': tag,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category = None).count(),
+        }
+    )
+
 
 
 def category_page(request, slug):
@@ -38,6 +54,7 @@ def category_page(request, slug):
         }
 
     )
+
 
 
 class PostDetail(DetailView):
